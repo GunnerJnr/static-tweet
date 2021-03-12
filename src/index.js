9f810de35f27a0d3340ed610e4dfe23284eb17e0
David Gunner <gunnerjnr@live.co.uk>
2021-03-12 21:26:29 +0000

    create components for avatar, author & message

--- src/index.js
+++ src/index.js
@@ -6,8 +6,38 @@ function Tweet ()
 {
   return (
     <div className="tweet">
-      Tweet
+      <Avatar />
+      <div className="content">
+        <Author />
+        <Message />
+      </div>
     </div>
+  );
+}
+
+function Avatar ()
+{
+  return (
+    <img src="https://www.gravatar.com/avatar/02a48bbe949fd860327e8d9ade169c9a" className="avatar" alt="avatar" />
+  );
+}
+
+function Message ()
+{
+  return (
+    <div className="message">
+      This is less than 140 characters.
+    </div>
+  );
+}
+
+function Author ()
+{
+  return (
+    <span className="author">
+      <span className="name">David Gunner</span>
+      <span className="handle">@gunnerjnr</span>
+    </span>
   );
 }
 
