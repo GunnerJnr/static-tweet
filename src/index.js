23f50775ca329bc7a9f226244fed57ee2576526f
David Gunner <gunnerjnr@live.co.uk>
2021-03-12 21:13:50 +0000

    create the styling for the tweet comp

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Tweet ()
+{
+  return (
+    <div className="tweet">
+      Tweet
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Tweet />,
+  document.querySelector( '#root' )
+);
